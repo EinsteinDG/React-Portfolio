6dacaf022fb2bdfadb3c04e066e8ecbf5c0cc9e9
EinsteinDG <enty84@gmail.com>
2021-08-26 00:19:52 -0500

    linked cards, profiles and added more imgs

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -12,7 +12,7 @@ function HomeCard() {
                     <div className="card-body">
                         <img src={HomeProfile} alt="home-profile-photo" className="ome-profile-photo mb-3"></img>
                         <h5 className="card-title text-center">Full Stack Web developer</h5>
-                        <p className="card-text text-center">Enterpreneur.</p>
+                        <p className="card-text text-center">Tech lover and Enterpreneur.</p>
                         <a href="https://www.linkedin.com/in/einstein-dalmau-07004710b/" className="ml-5">LinkedIn</a>
                         <a href="https://github.com/EinsteinDG" className="ml-5">Github</a>
                     </div>
